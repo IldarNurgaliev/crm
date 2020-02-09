dc5cb7f7d85b34123647a0c545a44a893d826444
Eldar <catshomee@gmail.com>
2020-02-09 16:58:47 +0300

    add some layouts

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,30 @@
 <template>
   <div id="app">
-    <router-view />
+    <component :is="layout">
+      <router-view />
+    </component>
   </div>
 </template>
 
-<style></style>
+<script>
+import EmptyLayout from "@/layouts/EmptyLayout";
+import MainLayout from "@/layouts/MainLayout";
+
+export default {
+  computed: {
+    // eslint-disable-next-line vue/return-in-computed-property
+    layout() {
+      console.log(this.$route.meta.layout);
+      return (this.$route.meta.layout || "empty") + "-layout";
+    }
+  },
+  components: {
+    EmptyLayout,
+    MainLayout
+  }
+};
+</script>
+<style lang="scss">
+@import "~materialize-css/dist/css/materialize.min.css";
+@import "assets/index.css";
+</style>
